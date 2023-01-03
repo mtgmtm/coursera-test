44cd9a65d2188768aad8e93c2d47411d2c80c81e
mtgmtm <kmartin9288@outlook.hu>
2023-01-03 13:57:25 +0100

    updatet mod4 solution

--- mod4_solution/js/script.js
+++ mod4_solution/js/script.js
@@ -1,20 +1,13 @@
-var names = new Array();
-names[0] = "János";
-names[1] = "József";
-names[2] = "Imre";
-names[3] = "Kelemen";
-names[4] = "Júlia";
-names[5] = "Virág";
-names[6] = "Jácint";
-names[7] = "István";
-names[8] = "Margit";
-names[9] = "Nimród";
+(function (){
+    var names = ["Jácint", "János", "Kelemen", "Imre", "Margit", "Ferenc", "Jácint", "Ilona", "Júlia", "Nimród"];
 
-for (var i = 0; i < names.length; i++) {
-    if(names[i].charAt(0) === 'J'|| names[i].charAt(0) === 'j'){
-        console.log("Goodbye " + names[i])
-    } 
-    else {
-        console.log("Hello " + names[i])
+    for (i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt().toLowerCase();
+      if (firstLetter =="j") {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
     }
-}
+    
+    })();
